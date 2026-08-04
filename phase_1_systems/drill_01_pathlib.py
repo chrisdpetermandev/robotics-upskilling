23f6d9f9b10b3dceb9f084d73f8f5c60e9a71b9e
chrisdpetermandev <chrisdpetermandev@gmail.com>
2026-08-04 19:08:14 -0400

    feat(phase1): completed pathlib file scanner drill

---
+++ phase_1_systems/drill_01_pathlib.py
@@ -0,0 +1,26 @@
+from pathlib import Path
+from typing import Iterator
+
+def get_parent_directory() -> Path:
+    log_dir = Path.home() / "Desktop" / "Robotics-Upskilling"
+    if log_dir.exists():
+        return log_dir
+    else:
+        print("[ERROR]: Could not find Path.home()/Desktop/Robotics-Upskilling")
+        raise FileNotFoundError
+        
+
+def scan_markdown_files(log_dir: Path) -> None:
+    try:
+        markdown_files = log_dir.rglob("*.md")
+
+        for markdown_file in markdown_files:
+            print(f"FILE NAME: {markdown_file.name}")
+            print(f"FILE PARENT: {markdown_file.parent.name}")
+            print(f"FILE SIZE(BYTES): {markdown_file.stat().st_size}")
+    except Exception as e:
+        print(f"[ERROR]: Could not locate .md files\n[MESSAGE]{e}")
+
+
+log_file = get_parent_directory()
+scan_markdown_files(log_file)
